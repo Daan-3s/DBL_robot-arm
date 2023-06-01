18d970a85b8cdc4931de4c70a393bf9be30873bd
OKlarenbeek <o.p.klarenbeek@student.tue.nl>
2023-06-01 21:57:10 +0200

    Update SimpleShapeIt.m

--- ShapeIt/Feedback Controllers/SimpleShapeIt.m
+++ ShapeIt/Feedback Controllers/SimpleShapeIt.m
@@ -1,10 +1,13 @@
 clear all
 
 [FRF_R1, hz_R1] = RTScope_figure2FRF_data('FRF_RmotorOpen2');
-FRF_R1_new = FRF_R1;
 
-[FRF_R1, hz_R1] = RTScope_figure2FRF_data('FRF_RmotorOpen2');
-FRF_R1_cut= FRF_R1(1:200);
-hz_R1_cut=hz_R1(1:200);
+[FRF_X1, hz_X1] = RTScope_figure2FRF_data('FRF_XmotorOpen2');
+
+[FRF_Z1, hz_Z1] = RTScope_figure2FRF_data('FRF_ZmotorOpen2');
+
+% [FRF_r1, hz_R1] = RTScope_figure2FRF_data('FRF_RmotorOpen2');
+% FRF_R1_cut= FRF_R1(1:200);
+% hz_R1_cut=hz_R1(1:200);
 
 shapeit
